398fa4e4182f3abac4ce358a39cc7cfe2e4f9fb1
Serve63 <Zakelijk@theimpactbox.co>
2026-06-04 11:49:10 +0200

    Improve public SEO metadata image trust
    Replace weak generated-looking public SEO metadata image defaults and guard rendered metadata output.

--- server/services/public-seo.js
+++ server/services/public-seo.js
@@ -1,5 +1,5 @@
 const DEFAULT_SITE_ORIGIN = 'https://www.softora.nl';
-const DEFAULT_OG_IMAGE_PATH = '/assets/softora-office-digital-growth.jpg';
+const DEFAULT_OG_IMAGE_PATH = '/assets/seo-content/website-leads-analytics-softora.jpg';
 const DEFAULT_LOGO_PATH = '/assets/61C2BCF5-70E9-4789-AFDE-FA18C862D58A.PNG';
 const SOFTORA_PUBLIC_EMAIL = 'info@softora.nl';
 const SOFTORA_PUBLIC_PHONE = '+31643262792';
