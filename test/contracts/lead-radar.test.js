bc0a269d278e34842bcb65d45ac6e5272d7a9c51
Serve63 <Zakelijk@theimpactbox.co>
2026-08-18 18:20:24 +0200

    test: import Lead Radar public post metadata helper

--- test/contracts/lead-radar.test.js
+++ test/contracts/lead-radar.test.js
@@ -8,6 +8,7 @@ const {
   buildSearchPlan,
   buildSignalFromProviderItem,
   classifySignal,
+  getPublicPagePublicationDetails,
   createLeadRadarService,
   hasCompletedInitialBackfill,
   isLikelyDirectPlatformPostUrl,
